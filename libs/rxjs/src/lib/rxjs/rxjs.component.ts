461b7e20959b8015d7191134b626d090021f03d5
Albuisson Stéphane <stephane.albuisson@gmail.com>
2024-11-12 13:42:45 +0100

    Initialisation du projet

---
+++ libs/rxjs/src/lib/rxjs/rxjs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-rxjs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rxjs.component.html',
+  styleUrl: './rxjs.component.css',
+})
+export class RxjsComponent {}
